5f836f996d600a58ff9ba78819f9eef0e7966fb1
ShawnXie <shawn0326@163.com>
2018-04-04 18:09:49 +0800

    add new files for Super SSAO

---
+++ three/js/custom/SuperSAOShader.js
@@ -0,0 +1,31 @@
+/**
+ * @author shawn / http://www.halflab.me
+ *
+ * Super SAO shader
+ */
+
+THREE.SuperSAOShader = {
+
+	uniforms: {},
+
+	vertexShader: [
+
+		"void main() {",
+
+			"gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );",
+
+		"}"
+
+	].join( "\n" ),
+
+	fragmentShader: [
+
+		"void main() {",
+
+			"gl_FragColor = vec4( 1.0, 0.0, 0.0, 0.5 );",
+
+		"}"
+
+	].join( "\n" )
+
+};
